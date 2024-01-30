2a5f80bd005ab3d50753d0bd041cef9d08285f29
unknown <brendanhuddleston18@gmail.com>
2024-01-29 20:08:36 -0600

    refactored

--- api/capital_finder.py
+++ api/capital_finder.py
@@ -12,17 +12,16 @@ class handler(BaseHTTPRequestHandler):
 
     if "country" in country_dictionary:
       response = requests.get(f"https://restcountries.com/v3.1/name/{country_dictionary['country']}?fullText=true")
-      # response = requests.get(f"https://restcountries.com/v3.1/name/Germany?fullText=true")
       response_json = response.json()
-      capitals = []
       capital = response_json[0]["capital"][0]
-      capitals.append(capital)
       message = f"The capital of {country_dictionary['country']} is {capital}"
+
     elif "capital" in country_dictionary:
       response = requests.get(f"https://restcountries.com/v3.1/capital/{country_dictionary['capital']}")
       response_json = response.json()
       country = response_json[0]["name"]["common"]
       message = f"{country_dictionary['capital']} is the capital of {country}"
+
     else:
       message = "Please enter a valid capital or country"
 
